b0e914ad453fd2091f8ae1ba54c7c4e7a16a6da1
aaron-kotch <aaronssikua@gmail.com>
2021-07-19 19:05:17 +0800

    - Added splash screen - Completed settings.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:oneshot/settings.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,39 +10,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    //sets status bar and nav bar color
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        systemNavigationBarColor: Color(0xff1E1E1E),
+        statusBarColor: Color(0xff1E1E1E),
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarIconBrightness: Brightness.light
+    ));
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -49,29 +35,61 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return SafeArea(
         child: Scaffold(
           backgroundColor: Color(0xff1E1E1E),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-
-              ],
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(top: 32, left: 32, right: 32),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(
+                        Icons.menu,
+                        size: 25,
+                        color: Colors.white,
+                      ),
+                      Container(
+                        child: Text(
+                          'Projects',
+                          style: TextStyle(
+                            fontFamily: 'Calibri',
+                            fontWeight: FontWeight.w300,
+                            fontSize: 24,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.settings,
+                          size: 25,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(toSettings());
+                        },
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
           floatingActionButton: FloatingActionButton(
+            backgroundColor: Color(0xffD4410D),
             onPressed:() {
               add();
             },
             tooltip: 'Increment',
-            child: Icon(Icons.add),
+            child: Icon(Icons.add, color: Colors.black,
+            ),
           ), // This trailing comma makes auto-formatting nicer for build methods.
         )
     );
@@ -82,4 +100,21 @@ class _MyHomePageState extends State<MyHomePage> {
     return 0;
 
   }
+}
+
+Route toSettings() {
+  return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => Settings(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(1, 0);
+        var end = Offset.zero;
+        var curve = Curves.easeOut;
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      }
+  );
 }
---
+++ lib/settings.dart
@@ -0,0 +1,372 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:oneshot/main.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({Key? key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    Color orangeAccent = Color(0xffD5754A);
+
+    return SafeArea(
+        child: Scaffold(
+          backgroundColor: Color(0xff1E1E1E),
+          body: Container(
+            height: height,
+            width: width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(top: 32, left: 4, bottom: 32),
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back_ios_rounded),
+                    iconSize: 20,
+                    color: Colors.white,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 20, bottom: 12),
+                  child: Text(
+                    "STORAGE / BACKUP",
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                      color: orangeAccent
+                    ),
+                  ),
+                ), // storage section
+                Container(
+                  width: width,
+                  alignment: Alignment.centerLeft,
+                  child: TextButton.icon(
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                      side: BorderSide(
+                        color: Color(0xff464646),
+                        width: 0.2
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                      )
+                    ),
+                    icon: Icon(
+                      Icons.cloud,
+                      size: 20,
+                      color: orangeAccent,
+                    ),
+                    label: Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 24),
+                        child: Text(
+                          'iCloud Sync',
+                          style: TextStyle(
+                            fontFamily: 'CenturyGothic',
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+
+                    },
+                  )
+                ), // icloudSync button
+                Container(
+                    width: width,
+                    alignment: Alignment.centerLeft,
+                    child: TextButton.icon(
+                      style: TextButton.styleFrom(
+                          padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                          side: BorderSide(
+                              color: Color(0xff464646),
+                              width: 0.2
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          )
+                      ),
+                      icon: ImageIcon(
+                        AssetImage("assets/icons/google_drive.png"),
+                        size: 20,
+                        color: orangeAccent,
+                      ),
+                      label: Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 24),
+                          child: Text(
+                            'Google Drive',
+                            style: TextStyle(
+                              fontFamily: 'CenturyGothic',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+
+                      },
+                    )
+                ), // google drive button
+                Container(
+                    width: width,
+                    alignment: Alignment.centerLeft,
+                    child: TextButton.icon(
+                      style: TextButton.styleFrom(
+                          padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                          side: BorderSide(
+                              color: Color(0xff464646),
+                              width: 0.2
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          )
+                      ),
+                      icon: ImageIcon(
+                        AssetImage("assets/icons/onedrive.png"),
+                        size: 20,
+                        color: orangeAccent,
+                      ),
+                      label: Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 24),
+                          child: Text(
+                            'iCloud Sync',
+                            style: TextStyle(
+                              fontFamily: 'CenturyGothic',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+
+                      },
+                    )
+                ), // onedrive button
+                Container(
+                  padding: EdgeInsets.only(top: 32, left: 20, bottom: 12),
+                  child: Text(
+                    "PRIVACY",
+                    style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18,
+                        color: orangeAccent
+                    ),
+                  ),
+                ), // privacy section
+                Container(
+                    width: width,
+                    alignment: Alignment.centerLeft,
+                    child: TextButton.icon(
+                      style: TextButton.styleFrom(
+                          padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                          side: BorderSide(
+                              color: Color(0xff464646),
+                              width: 0.2
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          )
+                      ),
+                      icon: ImageIcon(
+                        AssetImage("assets/icons/face_id.png"),
+                        size: 20,
+                        color: orangeAccent,
+                      ),
+                      label: Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 24),
+                          child: Text(
+                            'Face ID',
+                            style: TextStyle(
+                              fontFamily: 'CenturyGothic',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+
+                      },
+                    )
+                ), // face id button
+                Container(
+                    width: width,
+                    alignment: Alignment.centerLeft,
+                    child: TextButton.icon(
+                      style: TextButton.styleFrom(
+                          padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                          side: BorderSide(
+                              color: Color(0xff464646),
+                              width: 0.2
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          )
+                      ),
+                      icon: ImageIcon(
+                        AssetImage("assets/icons/password.png"),
+                        size: 20,
+                        color: orangeAccent,
+                      ),
+                      label: Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 24),
+                          child: Text(
+                            'Passcode',
+                            style: TextStyle(
+                              fontFamily: 'CenturyGothic',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+
+                      },
+                    )
+                ), // passcode button
+                Container(
+                  padding: EdgeInsets.only(top: 32, left: 20, bottom: 12),
+                  child: Text(
+                    "DISPLAY",
+                    style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18,
+                        color: orangeAccent
+                    ),
+                  ),
+                ), // privacy section
+                Container(
+                    width: width,
+                    alignment: Alignment.centerLeft,
+                    child: TextButton.icon(
+                      style: TextButton.styleFrom(
+                          padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                          side: BorderSide(
+                              color: Color(0xff464646),
+                              width: 0.2
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          )
+                      ),
+                      icon: ImageIcon(
+                        AssetImage("assets/icons/moon.png"),
+                        size: 20,
+                        color: orangeAccent,
+                      ),
+                      label: Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 24),
+                          child: Text(
+                            'Face ID',
+                            style: TextStyle(
+                              fontFamily: 'CenturyGothic',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+
+                      },
+                    )
+                ), // face id button
+                Container(
+                    width: width,
+                    alignment: Alignment.centerLeft,
+                    child: TextButton.icon(
+                      style: TextButton.styleFrom(
+                          padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                          side: BorderSide(
+                              color: Color(0xff464646),
+                              width: 0.2
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                          )
+                      ),
+                      icon: Icon(
+                        Icons.language,
+                        size: 20,
+                        color: orangeAccent,
+                      ),
+                      label: Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 24),
+                          child: Text(
+                            'Language',
+                            style: TextStyle(
+                              fontFamily: 'CenturyGothic',
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+
+                      },
+                    )
+                ), // passcode button
+              ],
+            ),
+          ),
+        )
+    );
+  }
+
+  void test() {
+
+  }
+}
+
+Route toMain() {
+  return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => MyHomePage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(-1, 0);
+        var end = Offset.zero;
+        var curve = Curves.easeInQuad;
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      }
+  );
+}
